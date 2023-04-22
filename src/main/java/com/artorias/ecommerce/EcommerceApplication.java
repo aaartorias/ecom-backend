4d260079cdbb32159f3ecad9a41ac9e44155f4b1
aaartorias <rajputshubhendu@gmail.com>
2023-04-23 01:03:56 +0530

    ecommerce:be search for products by categoryId

--- src/main/java/com/artorias/ecommerce/EcommerceApplication.java
+++ src/main/java/com/artorias/ecommerce/EcommerceApplication.java
@@ -1,13 +1,44 @@
 package com.artorias.ecommerce;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.impl.StaticLoggerBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
+@Slf4j
 public class EcommerceApplication {
 
+
 	public static void main(String[] args) {
+		final StaticLoggerBinder binder = StaticLoggerBinder.getSingleton();
+		log.info("a----" + binder.getLoggerFactory().toString());
+		log.info("b----" +binder.getLoggerFactoryClassStr());
 		SpringApplication.run(EcommerceApplication.class, args);
+//	static Map<Integer, Integer> mp1 = new HashMap<>();
+//	static Map<Integer, Integer> mp2 = new HashMap<>();
+//	mp1.put(1,10);
+//		mp1.put(4,40);
+//		mp1.put(5,900);
+//
+//		print(mp1);
+//
+//		mp2.put(5,50);
+//		mp2.put(8,80);
+//
+////		mp2.putAll(mp1);
+//		mp1.putAll(mp2);
+
+//		print(mp2);
+//		print(mp1);
+
+	}
+
+	static <T>void print(T message) {
+		log.info((String)message);
 	}
 
 }
